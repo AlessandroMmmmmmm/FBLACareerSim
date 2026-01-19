b72ae576d054416870a33659079dbe24ccdf90cb
Alessandro Montesi <alessandromontesi234@gmail.com>
2026-01-18 21:42:03 -0800

    working on eng

--- 2D_IsoTilemaps_Project/Assets/Scripts/SoftEng/ProgramManager.cs
+++ 2D_IsoTilemaps_Project/Assets/Scripts/SoftEng/ProgramManager.cs
@@ -1,29 +1,41 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ProgramManager : MonoBehaviour
 {
-    // This is your "ProgramList"
     public List<string> program = new List<string>();
+
+    [Header("UI")]
+    public Transform contentParent;
+    public GameObject programLinePrefab;
+
+    private List<GameObject> uiLines = new List<GameObject>();
 
     public void AddMove(string move)
     {
         program.Add(move);
-        Debug.Log("Added: " + move);
-    }
 
+        GameObject line = Instantiate(programLinePrefab, contentParent);
+        line.GetComponentInChildren<TextMeshProUGUI>().text = move;
+        Debug.Log(move);
+        uiLines.Add(line);
+    }
+    
     public void DeleteLastMove()
     {
-        if (program.Count > 0)
-        {
-            program.RemoveAt(program.Count - 1);
-            Debug.Log("Removed");
-        }
+        if (program.Count == 0) return;
+
+        program.RemoveAt(program.Count - 1);
+
+        GameObject lastLine = uiLines[uiLines.Count - 1];
+        uiLines.RemoveAt(uiLines.Count - 1);
+        Destroy(lastLine);
     }
 
     public void RunProgram()
     {
-        Debug.Log("Running program...");
+        Debug.Log("Running program:");
         foreach (string move in program)
         {
             Debug.Log(move);
